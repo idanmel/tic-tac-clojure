e3f0bf1d66fc78df605490190e3d2cd5a059c64c
Idan Melamed <idanmel@gmail.com>
2021-01-07 23:39:26 +0200

    refactoring

--- src/tic_tac_clojure/core.clj
+++ src/tic_tac_clojure/core.clj
@@ -21,14 +21,23 @@
 
 
 (defn valid-move? [state move]
+  "It's a valid move if there's an empty space there"
   (let [{:keys [board]} state]
     (= -1 (get-in board move))))
 
 
 (defn won? [board cells]
-  (= 1 (count (set (map #(get-in board %) cells)))))
+  "One of these spaces is known to be occupied.
+  There's a winner if all the cells are occupied
+  by the same player"
+  (= 1 (->> cells                                           ; ['(0 0) '(1 1) '(2 2)]
+            (map #(get-in board %))                         ; [\X \X \X]
+            set                                             ; #{\X}
+            count)))                                        ; 1
+
 
 (defn get-winning-coordinates [board [row-number col-number]]
+  "Return the winning coordinates"
   (let [row (map #(conj [row-number] %) (range 3))
         col (map #(conj '() col-number %) (range 3))
         diagonal ['(0 0) '(1 1) '(2 2)]
@@ -40,20 +49,30 @@
       (won? board reverse-diagonal) reverse-diagonal
       :else nil)))
 
+
 (defn win-status [board coordinates]
+  "Return the winning status"
   (cond
     (= \X (get-in board (first coordinates))) :x-won
     (= \O (get-in board (first coordinates))) :o-won
     :else nil))
 
+
+(defn board-count [board]
+  "Count the number of cells in the board"
+  (reduce + (map count board)))
+
+
 (defn draw-status [board moves]
   "Count the number of moves made and equal it to the number
   of places on the board"
-  (if (= (inc (count moves)) (reduce + (map count board)))
+  (if (= (inc (count moves)) (board-count board))
     :draw
-    :ok))
+    nil))
+
 
 (defn place-on-board [state move]
+  "Place the move on the board and return the appropriate status"
   (let [{:keys [board moves]} state
         new-board (assoc-in board move (player-symbol state))
         winning-coordinates (get-winning-coordinates new-board move)]
@@ -61,26 +80,16 @@
                  :board new-board
                  :winning-coordinates winning-coordinates
                  :status (or (win-status new-board winning-coordinates)
-                             (draw-status board moves)))))
+                             (draw-status board moves)
+                             :ok))))
 
 
 (defn turn [state move]
   "Given a state and the next move, returns the next state"
-  (cond
-    (empty? move) (assoc state :status :empty-move)
-    (valid-move? state move) (place-on-board state move)
-    (already-occupied? state move) (assoc state :status :already-occupied)
-    (out-of-bounds? state move) (assoc state :status :out-of-bounds)))
-
-
-(def init-state2
-  {:players "XO"
-   :moves []
-   :board [[-1 -1 -1]
-           [-1 -1 -1]
-           [-1 -1 -1]]
-   :status :ok
-   :winning-coordinates nil})
-
-(turn init-state2 '(0 0))
-(reduce turn init-state2 ['(0 0) '(0 1) '(1 1) '(2 2) '(1 2) '(1 0) '(0 2) '(2 0) '(2 1)])
+  (let [{:keys [board moves]} state]
+    (cond
+      (empty? move) (assoc state :status :empty-move)
+      (= (count moves) (board-count board)) (assoc state :status :too-many-moves)
+      (valid-move? state move) (place-on-board state move)
+      (already-occupied? state move) (assoc state :status :already-occupied)
+      (out-of-bounds? state move) (assoc state :status :out-of-bounds))))
--- test/tic_tac_clojure/core_test.clj
+++ test/tic_tac_clojure/core_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [tic-tac-clojure.core :refer :all]))
 
+
 (def init-state
   {:players "XO"
    :moves []
@@ -11,66 +12,80 @@
    :status :ok
    :winning-coordinates nil})
 
-(deftest empty-move
-  (testing "What happens when we call turn with an empty move?"
-    (is (= (turn init-state '())
-           (assoc init-state :status :empty-move)))))
 
 (deftest valid-move
   (testing "State changes correctly with valid move"
-    (is (= (turn init-state '(1 1))
-           (assoc init-state
-             :moves ['(1 1)]
-             :board [[-1 -1 -1] [-1 \X -1] [-1 -1 -1]])))))
+    (let [moves ['(1 1)]]
+      (is (= (turn init-state (first moves))
+             (assoc init-state
+               :moves moves
+               :board [[-1 -1 -1] [-1 \X -1] [-1 -1 -1]]))))))
+
 
 (deftest invalid-moves
+  (testing "What happens when we call turn with an empty move?"
+    (is (= (turn init-state '())
+           (assoc init-state :status :empty-move))))
   (testing "Trying to move to a cell that was already occupied"
-    (is (= (reduce turn init-state ['(1 1) '(1 1)])
-           (assoc init-state
-             :moves ['(1 1)]
-             :board [[-1 -1 -1] [-1 \X -1] [-1 -1 -1]]
-             :status :already-occupied))))
+    (let [moves ['(1 1) '(1 1)]]
+      (is (= (reduce turn init-state moves)
+             (assoc init-state
+               :moves [(first moves)]
+               :board [[-1 -1 -1] [-1 \X -1] [-1 -1 -1]]
+               :status :already-occupied)))))
   (testing "Trying to move to a cell that is not in the board"
     (is (= (turn init-state '(0 12))
-           (assoc init-state :status :out-of-bounds)))))
+           (assoc init-state :status :out-of-bounds))))
+  (testing "Too many moves"
+    (let [moves ['(0 0) '(0 1) '(1 1) '(2 2) '(1 2) '(1 0) '(0 2) '(2 0) '(2 1) '(0 0)]]
+      (is (= (reduce turn init-state moves)
+             (assoc init-state
+               :moves (drop-last moves)
+               :board [[\X \O \X] [\O \X \X] [\O \X \O]]
+               :status :too-many-moves))))))
 
 
 (deftest winning
   (testing "X had 3 in a row"
-    (is (= (reduce turn init-state ['(0 0) '(1 0) '(0 1) '(1 1) '(0 2)])
-           (assoc init-state
-             :moves ['(0 0) '(1 0) '(0 1) '(1 1) '(0 2)]
-             :board [[\X \X \X] [\O \O -1] [-1 -1 -1]]
-             :status :x-won
-             :winning-coordinates ['(0 0) '(0 1) '(0 2)]))))
+    (let [moves ['(0 0) '(1 0) '(0 1) '(1 1) '(0 2)]]
+      (is (= (reduce turn init-state moves)
+             (assoc init-state
+               :moves moves
+               :board [[\X \X \X] [\O \O -1] [-1 -1 -1]]
+               :status :x-won
+               :winning-coordinates ['(0 0) '(0 1) '(0 2)])))))
   (testing "O has 3 in a column!"
-    (is (= (reduce turn init-state ['(0 0) '(0 1) '(0 2) '(1 1) '(1 0) '(2 1)])
-           (assoc init-state
-             :moves ['(0 0) '(0 1) '(0 2) '(1 1) '(1 0) '(2 1)]
-             :board [[\X \O \X] [\X \O -1] [-1 \O -1]]
-             :status :o-won
-             :winning-coordinates ['(0 1) '(1 1) '(2 1)]))))
+    (let [moves ['(0 0) '(0 1) '(0 2) '(1 1) '(1 0) '(2 1)]]
+      (is (= (reduce turn init-state moves)
+             (assoc init-state
+               :moves moves
+               :board [[\X \O \X] [\X \O -1] [-1 \O -1]]
+               :status :o-won
+               :winning-coordinates ['(0 1) '(1 1) '(2 1)])))))
   (testing "X has 3 in the diagonal"
-    (is (= (reduce turn init-state ['(0 0) '(0 1) '(1 1) '(0 2) '(2 2)])
-           (assoc init-state
-             :moves ['(0 0) '(0 1) '(1 1) '(0 2) '(2 2)]
-             :board [[\X \O \O] [-1 \X -1] [-1 -1 \X]]
-             :status :x-won
-             :winning-coordinates ['(0 0) '(1 1) '(2 2)]))))
+    (let [moves ['(0 0) '(0 1) '(1 1) '(0 2) '(2 2)]]
+      (is (= (reduce turn init-state moves)
+             (assoc init-state
+               :moves moves
+               :board [[\X \O \O] [-1 \X -1] [-1 -1 \X]]
+               :status :x-won
+               :winning-coordinates ['(0 0) '(1 1) '(2 2)])))))
   (testing "O has 3 in the reverse diagonal"
-    (is (= (reduce turn init-state ['(0 0) '(0 2) '(0 1) '(1 1) '(1 0) '(2 0)])
-           (assoc init-state
-             :moves ['(0 0) '(0 2) '(0 1) '(1 1) '(1 0) '(2 0)]
-             :board [[\X \X \O] [\X \O -1] [\O -1 -1]]
-             :status :o-won
-             :winning-coordinates ['(0 2) '(1 1) '(2 0)])))))
-
+    (let [moves ['(0 0) '(0 2) '(0 1) '(1 1) '(1 0) '(2 0)]]
+      (is (= (reduce turn init-state moves)
+             (assoc init-state
+               :moves moves
+               :board [[\X \X \O] [\X \O -1] [\O -1 -1]]
+               :status :o-won
+               :winning-coordinates ['(0 2) '(1 1) '(2 0)]))))))
 
 
 (deftest draw
   (testing "Game ending in a draw"
-    (is (= (reduce turn init-state ['(0 0) '(0 1) '(1 1) '(2 2) '(1 2) '(1 0) '(0 2) '(2 0) '(2 1)])
-           (assoc init-state
-             :moves ['(0 0) '(0 1) '(1 1) '(2 2) '(1 2) '(1 0) '(0 2) '(2 0) '(2 1)]
-             :board [[\X \O \X] [\O \X \X] [\O \X \O]]
-             :status :draw)))))
+    (let [moves ['(0 0) '(0 1) '(1 1) '(2 2) '(1 2) '(1 0) '(0 2) '(2 0) '(2 1)]]
+      (is (= (reduce turn init-state moves)
+             (assoc init-state
+               :moves moves
+               :board [[\X \O \X] [\O \X \X] [\O \X \O]]
+               :status :draw))))))
+
