5cbdc5e7c32a9d4fa9027d5b9bd39d54416e6987
Idan Melamed <idanmel@gmail.com>
2021-01-07 16:32:03 +0200

    added a status keyword to the state

--- src/tic_tac_clojure/core.clj
+++ src/tic_tac_clojure/core.clj
@@ -1,12 +1,6 @@
 (ns tic-tac-clojure.core
   (:require [clojure.string :as str]))
 
-(def init-state
-  {:players "XO"
-   :moves []
-   :board [[-1 -1 -1]
-           [-1 -1 -1]
-           [-1 -1 -1]]})
 
 (defn split [regex txt]
   (str/split txt regex))
@@ -20,24 +14,6 @@
     (catch Exception e :number-format-exception)))
 
 
-(defn check-valid-move [state move]
-  (let [{:keys [board]} state
-        cell (get-in board move)]
-    (case cell
-      nil :out-of-range
-      "X" :place-occupied
-      "O" :place-occupied
-      -1 move
-      :something-went-wrong)))
-
-
-(defn get-move [state input]
-  (let [move (parse-input input)]
-    (case move
-      :number-format-exception :number-format-exception
-      (check-valid-move state move))))
-
-
 (defn player-symbol [state]
   "Returns the player symbol to be placed on the board"
   (let [{:keys [players moves]} state]
@@ -46,22 +22,32 @@
          (take (inc (count moves)))
          last)))
 
+
+(defn already-occupied? [state move]
+  "Returns true when trying to play a cell that was already played before"
+  (let [{:keys [board]} state]
+    (char? (get-in board move))))
+
+
+(defn out-of-bounds? [state move]
+  "Returns true when trying to move out of bounds"
+  (nil? (get-in state move)))
+
+
+(defn valid-move? [state move]
+  (let [{:keys [board]} state]
+    (= -1 (get-in board move))))
+
+
 (defn turn [state move]
   "Given a state and the next move, returns the next state"
-  (let [{:keys [players moves board]} state]
-    (assoc state :moves (conj moves move)
-                 :board (assoc-in board move (player-symbol state)))))
-    ;{:players players
-    ; :moves   (conj moves move)
-    ; :board   (assoc-in board move (player-symbol state))}))
-
-
-(defn move-it [state]
-  (let [input (read-line)
-        move (get-move state input)]
-    (if (coll? move)
-      move
-      (move-it state))))
+  (let [{:keys [moves board]} state]
+    (cond
+      (empty? move) (assoc state :status :empty-move)
+      (valid-move? state move) (assoc state :moves (conj moves move)
+                                    :board (assoc-in board move (player-symbol state)))
+      (already-occupied? state move) (assoc state :status :already-occupied)
+      (out-of-bounds? state move) (assoc state :status :out-of-bounds))))
 
 (defn game-ended? [state]
   (let [{:keys [moves]} state]
@@ -69,10 +55,11 @@
       true
       false)))
 
-(defn -main
-  []
-  (loop [state init-state]
-    (println state)
-    (if (game-ended? state)
-      (println "game ended")
-      (recur (turn state (move-it state))))) )
+;(defn -main
+;  []
+;  (loop [state init-state]
+;    (println state)
+;    (if (game-ended? state)
+;      (println "game ended")
+;      (recur (turn state (move-it state))))) )
+;
--- test/tic_tac_clojure/core_test.clj
+++ test/tic_tac_clojure/core_test.clj
@@ -2,6 +2,35 @@
   (:require [clojure.test :refer :all]
             [tic-tac-clojure.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def init-state2
+  {:players "XO"
+   :moves []
+   :board [[-1 -1 -1]
+           [-1 -1 -1]
+           [-1 -1 -1]]
+   :status :ok})
+
+(deftest empty-move
+  (testing "What happens when we call turn with an empty move?"
+    (is (= (turn init-state2 '())
+           (assoc init-state2 :status :empty-move)))))
+
+(deftest valid-move
+  (testing "State changes correctly with valid move"
+    (is (= (turn init-state2 '(1 1))
+           (assoc init-state2
+             :moves ['(1 1)]
+             :board [[-1 -1 -1] [-1 \X -1] [-1 -1 -1]])))))
+
+(deftest invalid-moves
+  (testing "Trying to move to a cell that was already occupied"
+    (is (= (reduce turn init-state2 ['(1 1) '(1 1)])
+           (assoc init-state2
+             :moves ['(1 1)]
+             :board [[-1 -1 -1] [-1 \X -1] [-1 -1 -1]]
+             :status :already-occupied))))
+  (testing "Trying to move to a cell that is not in the board"
+    (is (= (turn init-state2 '(0 12))
+           (assoc init-state2 :status :out-of-bounds)))))
+
+
